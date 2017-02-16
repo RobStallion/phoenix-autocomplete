9b7f8d68cea7a471cb8ebe4c2880a3ec147442b7
Sam Houston <shouston3@me.com>
2017-02-16 17:53:55 +0000

    refactoring and getting the autocomplete to a working state

--- web/controllers/auto_controller.ex
+++ web/controllers/auto_controller.ex
@@ -5,9 +5,22 @@ defmodule Autocomplete.AutoController do
     render conn, "index.html", words: []
   end
 
-  def get_words(conn, %{"word" => word}) do
-    IO.inspect word
-    render conn, "index.html", words: ["words"]
+  defp matching_words(words, word) do
+    number = 10
+    length = String.length(word)
+
+    String.split(words, "\n")
+    |> Enum.map(&(String.downcase(&1)))
+    |> Enum.split_while(fn(x) -> String.slice(x, 0, length) != word end)
+    |> elem(1)
+    |> Enum.slice(0, number)
+    |> Enum.split_while(fn(x) -> String.slice(x, 0, length) == word end)
+    |> elem(0)
+  end
+
+  def get_words(conn, %{"word" => %{"search" => word}}) do
+    {:ok, words} = File.read("text.txt")
+    render conn, "index.html", words: matching_words(words, word)
   end
 end
 
